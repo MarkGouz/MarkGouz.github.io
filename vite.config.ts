1feccb34bde436509e3767f811ab27be9d4bc443
Mark Gouzoulis <markgouzoulis@Marks-MacBook-Pro-2.local>
2025-06-19 11:55:19 -0400

    fixed build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/MarkGouz.github.io/',
+  build: {
+    outDir: 'docs', // Output goes here
+  },
 })
